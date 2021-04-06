9593ab870cd335318d348298c6cef497740941a0
Oto Dušek <oto.dusek@ysoft.com>
2021-04-06 19:29:20 +0200

    Download information updated

--- Analogy.LogViewer.SerialPort/IAnalogy/DownloadInformation.cs
+++ Analogy.LogViewer.SerialPort/IAnalogy/DownloadInformation.cs
@@ -7,11 +7,11 @@ namespace Analogy.LogViewer.SerialPort.IAnalogy
 {
     public class DownloadInformation : Analogy.LogViewer.Template.AnalogyDownloadInformation
     {
-        protected override string RepositoryURL { get; set; } = "https://api.github.com/repos/Analogy-LogViewer/Analogy.LogViewer.gRPC";
+        protected override string RepositoryURL { get; set; } = "https://api.github.com/repos/oto313/Analogy.LogViewer.SerialPort";
         public override TargetFrameworkAttribute CurrentFrameworkAttribute { get; set; } = (TargetFrameworkAttribute)Assembly.GetExecutingAssembly().GetCustomAttribute(typeof(TargetFrameworkAttribute));
 
         public override Guid FactoryId { get; set; } = SerialPortFactory.Id;
-        public override string Name { get; set; } = "Serial port Log Server Data Provider";
+        public override string Name { get; set; } = "Serial port Data Provider";
 
         private string? _installedVersionNumber;
         public override string InstalledVersionNumber
